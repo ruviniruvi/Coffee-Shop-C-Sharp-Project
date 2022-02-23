186803eb36f2b337324bc007f65ce36aa774ad87
ruviniruvi <ruviniruvi@gmail.com>
2022-02-23 14:50:58 -0500

    Upload files

---
+++ All Files/CoffeeShop/CoffeeShop/clEmployeeList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace CoffeeShop
+{
+    public class clEmployeeList
+    {
+        private int _employee_id;
+        private string _employee_first_name;
+        private string _employee_last_name;
+
+        public int EMPLOYEE_ID
+        {
+            get { return _employee_id; }
+            set { _employee_id = value; }
+        }
+
+        public string EMPLOYEE_FIRST_NAME
+        {
+            get { return _employee_first_name; }
+            set { _employee_first_name = value; }
+        }
+
+        public string EMPLOYEE_LAST_NAME
+        {
+            get { return _employee_last_name; }
+            set { _employee_last_name = value; }
+        }
+
+
+
+        public List<clEmployeeList> GetAll()
+        {
+            List<clEmployeeList> results = new List<clEmployeeList>();
+
+
+            SqlConnection con = new SqlConnection("Data Source=RUVISE;Initial Catalog=COFFEE_SHOP;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("SELECT EMPLOYEE_ID,EMPLOYEE_FIRST_NAME, EMPLOYEE_LAST_NAME FROM  EMPLOYEES", con);
+            using (con)
+            {
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    clEmployeeList newEmployee = new clEmployeeList();
+                    newEmployee.EMPLOYEE_ID = (int)reader["EMPLOYEE_ID"];
+                    newEmployee.EMPLOYEE_FIRST_NAME = (string)reader["EMPLOYEE_FIRST_NAME"];
+                    newEmployee.EMPLOYEE_LAST_NAME = (string)reader["EMPLOYEE_LAST_NAME"];
+
+
+
+
+                    results.Add(newEmployee);
+                }
+            }
+            con.Close();
+            return results;
+        }
+    }
+}
---
+++ All Files/WebApplication1/WebApplication1/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" style="border: medium double #000000; background-color: #F3F5DD; width: 558px; height: 384px; font-family: 'Times New Roman', Times, serif; color: #CC0099;">
+        <div>
+            
+            <h2 style="font-family: 'Times New Roman', Times, serif; font-size: large; font-style: normal; font-variant: normal; color: #FF3399; position: absolute; z-index: auto; top: 18px; left: 178px; width: 209px; height: 19px; font-weight: bold;"> Employee Information </h2>
+
+        </div>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="ObjectDataSource1" BackColor="Ivory" BorderStyle="Double" BorderColor="#D291BC" BorderWidth="2px" CellPadding="5" Height="238px" style="margin-left: 62px; margin-top: 70px">
+            <Columns>
+                <asp:BoundField DataField="EMPLOYEE_ID" HeaderText="Employee ID" SortExpression="EMPLOYEE_ID" />
+                <asp:BoundField DataField="EMPLOYEE_FIRST_NAME" HeaderText="Employee First Name" SortExpression="EMPLOYEE_FIRST_NAME" />
+                <asp:BoundField DataField="EMPLOYEE_LAST_NAME" HeaderText="Employee Last Name" SortExpression="EMPLOYEE_LAST_NAME" />
+            </Columns>
+        </asp:GridView>
+        <asp:ObjectDataSource ID="ObjectDataSource1" runat="server" SelectMethod="GetAll" TypeName="CoffeeShop.clEmployeeList"></asp:ObjectDataSource>
+    </form>
+</body>
+</html>
